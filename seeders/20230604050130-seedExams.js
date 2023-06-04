efa9b5b70dab610d7161974e3f3e22dfb63fa7a2
zhgchristian <zhg.christian@gmail.com>
2023-06-04 12:08:11 +0700

    feat: added exams seeding, changing tables

---
+++ seeders/20230604050130-seedExams.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    const exams = require("../db/exam.json");
+    exams.forEach((el) => {
+      el.createdAt = el.updatedAt = new Date();
+    });
+    await queryInterface.bulkInsert("Exams", exams);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete("Exams", null);
+  },
+};
